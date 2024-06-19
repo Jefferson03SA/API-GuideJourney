a4e21e05e970b6a89cc47e43ea683acc9cb1cdc8
Jefferson03SA <jsantillana3@upao.edu.pe>
2024-06-19 18:47:59 -0500

    agregacion de swagger

--- GuideJourney-API/src/main/java/com/guidejourney/controllers/UserController.java
+++ GuideJourney-API/src/main/java/com/guidejourney/controllers/UserController.java
@@ -10,11 +10,12 @@ import com.guidejourney.model.dto.UserResponseDTO;
 import com.guidejourney.model.entities.User;
 import com.guidejourney.services.UserService;
 
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/users")
-
+@Api(tags = "Gestión de Usuarios")
 public class UserController {
 
     @Autowired
@@ -24,6 +25,7 @@ public class UserController {
     private UserMapper userMapper;
 
     @PostMapping("/register")
+    @ApiOperation(value = "Registrar un nuevo usuario", notes = "Proporcione correo electrónico y contraseña para registrar un nuevo usuario")
     public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserDTO userDTO) {
         User user = userService.registerUser(userDTO);
         UserResponseDTO responseDTO = userMapper.convertToResponseDto(user);
--- GuideJourney-API/src/main/java/com/guidejourney/model/dto/UserDTO.java
+++ GuideJourney-API/src/main/java/com/guidejourney/model/dto/UserDTO.java
@@ -1,18 +1,20 @@
 package com.guidejourney.model.dto;
 
 import lombok.Data;
-
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.*;
 
 @Data
+@ApiModel(description = "DTO para registrar un nuevo usuario")
 public class UserDTO {
 
     @NotBlank(message = "El correo electrónico es obligatorio")
     @Email(message = "El correo electrónico debe ser válido")
+    @ApiModelProperty(value = "Dirección de correo electrónico del usuario", example = "usuario@ejemplo.com", required = true)
     private String email;
 
     @NotBlank(message = "La contraseña es obligatoria")
-    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    @ApiModelProperty(value = "Contraseña del usuario", required = true)
     private String password;
-
 }
